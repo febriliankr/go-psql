0019a21d0e9b492bdb0a76a6899fafab9a4fbdd8
Febrilian Kristiawan <febriliankr@gmail.com>
2021-04-11 01:20:44 +0700

    enable ssl database connection

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ const (
 
 func OpenConnection() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=enable",
+		"password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
